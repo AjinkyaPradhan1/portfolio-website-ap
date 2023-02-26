b3abbf9d3973c896480a650398823847ffc7e4d9
AjinkyaPradhan1 <virat@gmail.com>
2023-02-26 10:17:36 +0530

    Basic body started

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,8 +7,27 @@ function Home() {
 
       {/* Navbar content and heading */}
         <div className="navbar">
-          <div className="navbar_text">Ajinkya Pradhan</div>
+          <div className="navbar_text">AP</div>
           <div className="navbar_content"><Navbar/></div>
+        </div>
+
+      {/*Body*/}
+        <div className="home_content">
+          <div className="left">
+
+            <div className="left_content">
+              <div className="left_content1">
+                <div className="p0">Hi.......</div>
+                <div className="p1">I'm, Ajinkya Pradhan</div>
+              </div>
+              <div className="left_content2">This is my Portfolio Website</div>
+            </div>
+            
+          </div>
+
+          <div className="right">
+          <div className="right_content"></div>
+          </div>
         </div>
 
       </div>
