c7b220436f374573a3be830f1c326df464cf7647
AjinkyaPradhan1 <virat@gmail.com>
2023-02-26 19:00:07 +0530

    Front End Skills Made

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -19,15 +19,63 @@ function Skills() {
 
           <div className="skills_content1">
             <div className="skills1">
-              <div className="one">
-                <img class="java" src="./images/java.png" alt="java"></img>
+
+
+              <div className="skills1_heading">Front-End Development Skills</div>
+              <div className="skills1_content">
+                
+                <div className="skills1_content_row1">
+                  <div className="row1_1">
+                    <div className="one">
+                        <img class="icons" src="./images/html5.png" alt="java"></img>
+                    </div>
+                    <div className="two">HTML5</div>
+                    <div className="three">Advanced</div>
+                  </div>
+
+                  <div className="row1_2">
+                    <div className="one">
+                        <img class="icons" src="./images/css.png" alt="java"></img>
+                    </div>
+                    <div className="two">CSS</div>
+                    <div className="three">Advanced</div>
+                  </div>
+
+                  <div className="row1_3">
+                    <div className="one">
+                        <img class="icons" src="./images/js.png" alt="java"></img>
+                    </div>
+                    <div className="two">Javascript</div>
+                    <div className="three">Intermediate</div>
+                  </div>
+
+                </div>
+                <div className="skills1_content_row2">
+                    <div className="row1_1">
+                      <div className="one">
+                          <img class="icons" src="./images/react.jpg" alt="java"></img>
+                      </div>
+                      <div className="two">React JS</div>
+                      <div className="three">Beginner</div>
+                    </div>
+
+                    <div className="row1_2">
+                        <div className="one">
+                            <img class="icons" src="./images/git.png" alt="java"></img>
+                        </div>
+                        <div className="two">Git</div>
+                        <div className="three">Intermediate</div>
+                    </div>
+                    <div className="row1_3"></div>
+                </div>
               </div>
-              <div className="two">Java</div>
+              
             </div>
 
             <div className="skills2">
-              
+                  
             </div>
+            
           </div>
 
           <div className="skills_content2">
