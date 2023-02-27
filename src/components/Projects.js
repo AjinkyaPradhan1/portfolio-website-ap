ecfca4b414356c8f7356484bbbd408ba14aca7dd
AjinkyaPradhan1 <virat@gmail.com>
2023-02-27 16:15:42 +0530

    Projects Started

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -16,33 +16,89 @@ function Projects() {
         
         <div className="projects_content">
           <div className="p_content_1">
-              <div className="p_content_1_image"></div>
+              <div className="p_content_1_image">
+                  <img class="image__img" src="./images/quizmaina.png" alt="Bricks"/>
+                  <div class="image__overlay image__overlay--primary">
+                    <div class="image__title">
+                      <button className="project"><a href="">Live Project</a></button>
+                      <button className="sCode"><a href="">Source Code</a></button>
+                    </div>
+                    
+                  </div>
+              </div>
               <div className="p_content_1_details">QuizMania React App</div>
                 
           </div>
           
           <div className="p_content_2">
-              <div className="p_content_1_image"></div>
+              <div className="p_content_1_image">
+                <img class="image__img" src="" alt="Bricks"/>
+                  <div class="image__overlay image__overlay--primary">
+                    <div class="image__title">
+                      <button className="project"><a href="">Live Project</a></button>
+                      <button className="sCode"><a href="">Source Code</a></button>
+                    </div>
+                    
+                  </div>
+                  
+              </div>
               <div className="p_content_1_details">Calculator React App</div>
           </div>
 
           <div className="p_content_3">
-              <div className="p_content_1_image"></div>
+                    <div className="p_content_1_image">
+                      <img class="image__img" src="" alt="Bricks"/>
+                      <div class="image__overlay image__overlay--primary">
+                      <div class="image__title">
+                        <button className="project"><a href="">Live Project</a></button>
+                        <button className="sCode"><a href="">Source Code</a></button>
+                      </div>
+                    
+                  </div>
+              </div>
               <div className="p_content_1_details">WriteBuddy</div>
           </div>
 
           <div className="p_content_4">
-              <div className="p_content_1_image"></div>
+              <div className="p_content_1_image">
+                  <img class="image__img" src="" alt="Bricks"/>
+                      <div class="image__overlay image__overlay--primary">
+                      <div class="image__title">
+                        <button className="project"><a href="">Live Project</a></button>
+                        <button className="sCode"><a href="">Source Code</a></button>
+                      </div>
+                    
+                  </div>
+              </div>
               <div className="p_content_1_details">Scientific Calculatrix</div>
           </div>
 
           <div className="p_content_5">
-              <div className="p_content_1_image"></div>
+              <div className="p_content_1_image">
+                  <img class="image__img" src="" alt="Bricks"/>
+                      <div class="image__overlay image__overlay--primary">
+                      <div class="image__title">
+                        <button className="project"><a href="">Live Project</a></button>
+                        <button className="sCode"><a href="">Source Code</a></button>
+                      </div>
+                    
+                  </div>
+              </div>
               <div className="p_content_1_details">Learning Hub Django App</div>
           </div>
 
           <div className="p_content_6">
-              <div className="p_content_1_image"></div>
+              <div className="p_content_1_image">
+
+              <img class="image__img" src="" alt="Bricks"/>
+                      <div class="image__overlay image__overlay--primary">
+                      <div class="image__title">
+                        <button className="project"><a href="">Live Project</a></button>
+                        <button className="sCode"><a href="">Source Code</a></button>
+                      </div>
+                    
+                  </div>
+              </div>
               <div className="p_content_1_details">Art Gallery Django App</div>
           </div>
 
