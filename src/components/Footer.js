2d057076907f1c1f3f85ab436e7bdce6d76b7b42
AjinkyaPradhan1 <virat@gmail.com>
2023-02-27 21:22:59 +0530

    Footer Strted

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer(){
+    return(
+        <div className="fMain">
+            <div>&copy;All Rights Reserved</div>
+            <div>Made with &#10084; By: Ajinkya Pradhan&#8482;</div>
+        </div>
+    );
+}
+
+export default Footer;
+
