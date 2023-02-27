d91738dd555dfcc0df559325ccbb22b7cd8f4e56
AjinkyaPradhan1 <virat@gmail.com>
2023-02-27 11:54:43 +0530

    contact us progress

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -11,7 +11,34 @@ function Contact() {
           <div className="navbar_text">AP</div>
           <div className="navbar_content"><Navbar/></div>
         </div>
-
+        <div className="contact_content">
+          <div className="contact_content_left">
+            <div className="ig1">
+              <img class="image__img" src="./images/ig.jpg" alt="Bricks"/>
+              <div class="image__overlay image__overlay--primary">
+                <div class="image__title"><a href="https://www.instagram.com/ajinkyapp_1/">Link</a></div>
+                
+              </div>
+            </div>
+            <div className="ig1">
+              <img class="image__img" src="./images/linkedin.png" alt="Bricks"/>
+              <div class="image__overlay image__overlay--primary">
+                <div class="image__title"><a href="https://www.linkedin.com/in/ajinkya-pradhan-1a141b229/">Link</a></div>
+              </div>
+            </div>
+            <div className="ig1">
+              <img class="image__img" src="./images/github.png" alt="Bricks"/>
+              <div class="image__overlay image__overlay--primary">
+                <div class="image__title"><a href="https://github.com/AjinkyaPradhan1">Link</a></div>
+              </div>
+            </div>
+          </div>
+          <div className="contact_content_right">
+            <div className="ig"></div>
+            <div className="ig"></div>
+            <div className="ig"></div>
+          </div>
+        </div>
       </div>
     );
   }
