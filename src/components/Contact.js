24e7dcd88288dafb64c84ebb2d352dd7818bb1db
AjinkyaPradhan1 <virat@gmail.com>
2023-02-27 16:59:20 +0530

    Projects Started

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -15,20 +15,20 @@ function Contact() {
           <div className="contact_content_left">
             <div className="ic1">
               <img class="image__img" src="./images/ig.jpg" alt="Bricks"/>
-              <div class="image__overlay image__overlay--primary">
+              <div class="image__overlay1 image__overlay1--primary">
                 <div class="image__title"><a href="https://www.instagram.com/ajinkyapp_1/">Link</a></div>
                 
               </div>
             </div>
             <div className="ic2">
               <img class="image__img" src="./images/linkedin.png" alt="Bricks"/>
-              <div class="image__overlay image__overlay--primary">
+              <div class="image__overlay1 image__overlay1--primary">
                 <div class="image__title"><a href="https://www.linkedin.com/in/ajinkya-pradhan-1a141b229/">Link</a></div>
               </div>
             </div>
             <div className="ic3">
               <img class="image__img" src="./images/github.png" alt="Bricks"/>
-              <div class="image__overlay image__overlay--primary">
+              <div class="image__overlay1 image__overlay1--primary">
                 <div class="image__title"><a href="https://github.com/AjinkyaPradhan1">Link</a></div>
               </div>
             </div>
@@ -36,7 +36,7 @@ function Contact() {
           <div className="contact_content_right">
             <div className="ic4">
               <img class="image__img" src="./images/gmail.png" alt="Bricks"/>
-              <div class="image__overlay image__overlay--primary">
+              <div class="image__overlay1 image__overlay1--primary">
                 <div class="image__title">Email</div>
                 <p class="image__description">
                 ajinkyappradhan@gmail.com
@@ -46,7 +46,7 @@ function Contact() {
             </div>
             <div className="ic5">
               <img class="image__img" src="./images/phone.png" alt="Bricks"/>
-              <div class="image__overlay image__overlay--primary">
+              <div class="image__overlay1 image__overlay1--primary">
               <div class="image__title">Mobile No.</div>
                 <p class="image__description">
                 9425451952
