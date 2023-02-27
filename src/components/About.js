2de80d6d2b399c6fdd364f39efa85a712d940775
AjinkyaPradhan1 <virat@gmail.com>
2023-02-27 10:25:26 +0530

    basic about completed

--- src/components/About.js
+++ src/components/About.js
@@ -13,11 +13,43 @@ function About() {
         </div>
 
         <div className="about_content">
-          <div className="left">
+          <div className="left_about">
             <img class="img_about" src="./images/img_mine.jpg" alt="img_mine"></img>
           </div>
-          <div className="right">
-          
+
+          <div className="right_about">
+              <div className="about_me_heading">About Me</div>
+              <div className="about_me_desc">
+              Believer of excellence, having the zeal to up-skill, being efficient and productive for the 
+              organization and develop as well as diversify my professional
+              skill-set. Looking forward to working as a Software Professional in 
+              an Esteemed Corporation to get exposure to national as well as international
+              clients.
+              </div>
+
+              <div className="key_value">
+                <div className="title">
+                  <div className="title_contents">
+                    Name: <br></br>
+                    City:<br></br>
+                    Date of Birth:<br></br>
+                    Current Address:<br></br>
+                    Languages Known:<br></br>
+                    Interests:<br></br>
+                  </div>
+                </div>
+                <div className="details">
+                  <div className="details_content">
+                    Ajinkya Pradhan <br></br>
+                    Indore<br></br>
+                    August 25,2001<br></br>
+                    Pune, India<br></br>
+                    English, Hindi, Marathi, Gujarati<br></br>
+                    Web Development, Software Development
+                  </div>
+                </div>
+              </div>
+              
           </div>
         </div>
         
