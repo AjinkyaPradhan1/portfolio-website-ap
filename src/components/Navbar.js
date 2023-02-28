19658a0f46b362dee7a06f7b3bebe7aa388222bb
AjinkyaPradhan1 <virat@gmail.com>
2023-02-28 10:25:43 +0530

    480px navbar done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,24 @@
 import './navbar.css';
 
+
 function Navbar(){
+    
+
     return(
         <div className="navbarMain">
             <div class="topnav">
-                <a class="active" href="/">Home</a>
-                <a href='/about'>About</a>
-                <a href='/skills'>Skills</a>
-                <a href='/projects'>Projects</a>
-                <a href='/contact'>Contact</a>
-                
-                
+                    <a class="active" href="/">Home</a>
+                    <a href='/about'>About</a>
+                    <a href='/skills'>Skills</a>
+                    <a href='/projects'>Projects</a>
+                    <a href='/contact'>Contact</a>
+                    {/* <div id="mobile">
+                        <i id="bar" className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}>
+                        </i>
+                    </div> */}
             </div>
         </div>
+        
     );
 }
 
