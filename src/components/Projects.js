3926db876559afd42e8f12671591d30374689459
Ajinkya <ap@gmail.com>
2023-08-31 12:58:54 +0530

    Edit Projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -19,7 +19,7 @@ function Projects() {
       
           <div className="p_content_1">
               <div className="p_content_1_image">
-                  <img class="image__img" src="./images/quizmaina.png" alt="Bricks"/>
+                  <img class="image__img" src="./images/quizmaina.png" alt="QuizMania"/>
                   <div class="image__overlay image__overlay--primary">
                     <div class="image__title">
                       <button className="qmania"><a href="http://quizmaniawapp.netlify.app">Live Project</a></button>
@@ -34,22 +34,22 @@ function Projects() {
           
           <div className="p_content_2">
               <div className="p_content_1_image">
-                <img class="image__img" src="./images/calc.png" alt="Bricks"/>
+                <img class="image__img" src="./images/todo.png" alt="toDo"/>
                   <div class="image__overlay image__overlay--primary">
                     <div class="image__title">
-                      <button className="calcButton"><a href="https://simplecalculatrix.netlify.app">Live Project</a></button>
-                      <button className="sCode"><a href="https://github.com/AjinkyaPradhan1/Calculator">Source Code</a></button>
+          
+                      <button className="project"><a href="https://github.com/AjinkyaPradhan1/ToDo-Full-Stack-App">Source Code</a></button>
                     </div>
                     
                   </div>
                   
               </div>
-              <div className="p_content_1_details">Calculator React App</div>
+              <div className="p_content_1_details">Full Stack ToDo</div>
           </div>
 
           <div className="p_content_3">
                     <div className="p_content_1_image">
-                      <img class="image__img" src="./images/writebuddy.jpg" alt="Bricks"/>
+                      <img class="image__img" src="./images/writebuddy.jpg" alt="writeBuddy"/>
                       <div class="image__overlay image__overlay--primary">
                       <div class="image__title">
                         <button className="project"><a href="https://play.google.com/store/apps/details?id=com.eduapp.writebuddy">Live Project</a></button>
@@ -63,7 +63,7 @@ function Projects() {
 
           <div className="p_content_4">
               <div className="p_content_1_image">
-                  <img class="image__img" src="./images/calcScient.jpg" alt="Bricks"/>
+                  <img class="image__img" src="./images/calcScient.jpg" alt="Scientific Calculatrix"/>
                       <div class="image__overlay image__overlay--primary">
                       <div class="image__title">
                         <button className="project"><a href="https://play.google.com/store/apps/details?id=com.mathsapp.scientificcalculatrix">Live Project</a></button>
@@ -77,7 +77,7 @@ function Projects() {
 
           <div className="p_content_5">
               <div className="p_content_1_image">
-                  <img class="image__img" src="./images/learning.jpg" alt="Bricks"/>
+                  <img class="image__img" src="./images/learning.jpg" alt="Learning Hub"/>
                       <div class="image__overlay image__overlay--primary">
                       <div class="image__title">
           
@@ -92,7 +92,7 @@ function Projects() {
           <div className="p_content_6">
               <div className="p_content_1_image">
 
-              <img class="image__img" src="./images/art.jpg" alt="Bricks"/>
+              <img class="image__img" src="./images/art.jpg" alt="Art Gallery"/>
                       <div class="image__overlay image__overlay--primary">
                       <div class="image__title">
                         
@@ -105,19 +105,30 @@ function Projects() {
           </div>
 
           <div className="p_content_7">
+                <div className="p_content_1_image">
+
+                      <img class="image__img" src="./images/calc.png" alt="React Calculator"/>
+                      <div class="image__overlay image__overlay--primary">
+                        <div class="image__title">
+                          
+                          <button className="calcButton"><a href="https://simplecalculatrix.netlify.app">Live Project</a></button>
+                          <button className="sCode"><a href="https://github.com/AjinkyaPradhan1/Calculator">Source Code</a></button>
+                        </div>
+                    
+                      </div>
+                </div>
+              <div className="p_content_1_details">React Calculator App</div>
+          </div>
+
+          <div className="p_content_8">
+          {/* New Added Here Starts */}
               <div className="p_content_2_image">
                         <button className="github"><a href="https://github.com/AjinkyaPradhan1/">Github</a></button><br></br>
                         <button className="gconsole"><a href="https://play.google.com/store/apps/developer?id=AJINKYA+PRADHAN">Google Play Console</a></button>
               </div>
               <div className="p_content_1_details">More Projects
               <img src="./images/arrow2.png" class="arrow" alt="arrow"></img></div>
-          </div>
-
-          <div className="p_content_8">
-              <div className="p_content_2_image">
-        
-              </div>
-              <div className="p_content_1_details"></div>
+              {/* New Added Here Ends */}
           </div>
 
           <div className="p_content_9">
