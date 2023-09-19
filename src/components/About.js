5ae2ac7f1e136d7f11e76d191979febabb085273
Ajinkya <ap@gmail.com>
2023-09-19 13:39:18 +0530

    about title head section

--- src/components/About.js
+++ src/components/About.js
@@ -30,23 +30,37 @@ function About() {
               <div className="key_value">
                 <div className="title">
                   <div className="title_contents">
-                    Name: <div className="title_contents_name">Ajinkya Pradhan</div><br></br>
-                    City: <div className="title_contents_city">Indore</div><br></br>
-                    Date of Birth:<div className="title_contents_dob">August 25,2001</div><br></br>
-                    Current Address:<div className="title_contents_ca">Indore, India</div><br></br>
-                    Languages Known:<div className="title_contents_langKnown">English, Hindi, Marathi, Gujarati</div><br></br>
-                    Interests:<div className="title_contents_interest">Full Stack Development, Java Developer , Web Developer</div><br></br>
-                    Hobbies:<div className="title_contents_hobbies">Learning languages, Football, Cricket, Painting, Reading Books</div><br></br>
+                    
+                    <div className="title_contents_nameHead">Name:</div><br/> 
+                    <div className="title_contents_name">Ajinkya Pradhan</div>
+                    
+                    <div className="title_contents_cityHead">City: </div><br/>
+                    <div className="title_contents_city">Indore</div>
+                    
+                    <div className="title_contents_dobHead">Date of Birth:</div><br/>
+                    <div className="title_contents_dob">August 25,2001</div>
+
+                    <div className="title_contents_caHead">Current Address:</div><br/>
+                    <div className="title_contents_ca">Indore, India</div>
+
+                    <div className="title_contents_langHead">Languages Known:</div><br/>
+                    <div className="title_contents_langKnown">English, Hindi, Marathi, Gujarati</div>
+
+                    <div className="title_contents_intHead">Interests:</div><br/>
+                    <div className="title_contents_interest">Full Stack Development, Java Developer , Web Developer</div>
+
+                    <div className="title_contents_hobbHead">Hobbies:</div><br/>
+                    <div className="title_contents_hobbies">Learning languages, Football, Cricket, Painting, Reading Books</div>
                   </div>
                 </div>
                 <div className="details">
                   <div className="details_content">
-                    Ajinkya Pradhan <br></br>
-                    Indore<br></br>
-                    August 25,2001<br></br>
-                    Indore, India<br></br>
-                    English, Hindi, Marathi, Gujarati<br></br>
-                    Full Stack Development, Java Developer , Web Developer<br></br>
+                    Ajinkya Pradhan <br/>
+                    Indore<br/>
+                    August 25,2001<br/>
+                    Indore, India<br/>
+                    English, Hindi, Marathi, Gujarati<br/>
+                    Full Stack Development, Java Developer , Web Developer<br/>
                     Learning languages, Football, Cricket, Painting, Reading Books
                   </div>
                 </div>
